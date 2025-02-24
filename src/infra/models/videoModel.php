22be6271d192d057e2eb97d9afd0ca3c37eda536
Pedro Celestino <pedrojuniorcelestinocg@gmail.com>
2025-02-24 17:08:11 -0400

    feat: começo do videoModal

---
+++ src/infra/models/videoModel.php
@@ -0,0 +1,35 @@
+<?php
+namespace Src\Infra\Models;
+
+require_once __DIR__ . '/../../infra/db/database.php';
+use Database;
+use PDO;
+
+class videoModel {
+    private PDO $connection;
+
+    public function __construct() {
+        $this->connection = (new Database())->getConnection();
+    }
+
+    public function listMostPopularVideo(): string {
+        $sql = "SELECT VIDEO.id_user ,VIDEO.nome AS video_nome, SUM(VIEW.views) AS total_views
+        FROM VIDEO
+        JOIN VIEW VIEW ON VIDEO.id = VIEW.id_video
+        GROUP BY VIDEO.id
+        ORDER BY total_views DESC";
+        $stmt = $this->connection->prepare($sql);
+        $stmt->execute();
+        return $stmt->fetch(PDO::FETCH_ASSOC);
+    }
+
+    public function findByEmail(string $email): array {
+        $sql = "SELECT * FROM users WHERE email = :email";
+
+        $stmt = $this->connection->prepare($sql);
+        $stmt->bindParam(":email", $email);
+        $stmt->execute();
+
+        return $stmt->fetch(PDO::FETCH_ASSOC);
+    }
+}
