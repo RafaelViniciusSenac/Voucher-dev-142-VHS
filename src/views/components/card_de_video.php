cd48c91cc7b23ef25bd694e27bdc8daa5d9e581a
Josué <josuealexandre067@gmail.com>
2025-02-24 17:11:46 -0400

    Update card_de_video.php

--- src/views/components/card_de_video.php
+++ src/views/components/card_de_video.php
@@ -11,8 +11,9 @@
         $duration = formatarDuracao($video['duration']);
         $visualizations = formatarVisualizacoes($video['visualizations']);
         $create_at = tempoDecorrido($video['create_at']);
-        
+
         return "
+            <h1></h1>
             <div class='relative w-[320px] h-[400px] bg-[#1e1e2a] rounded-3xl overflow-hidden shadow-lg hover:shadow-xl transition-all duration-300'>
                 
                 <!-- Capa do vídeo -->
