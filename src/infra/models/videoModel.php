f20fcd08aa8232c8d1b7b1d03b85925da673c8fc
Pedro Celestino <pedrojuniorcelestinocg@gmail.com>
2025-03-07 14:50:55 -0400

    feat: função listar video

--- src/infra/models/videoModel.php
+++ src/infra/models/videoModel.php
@@ -15,21 +15,11 @@ class videoModel {
     public function listMostPopularVideo(): string {
         $sql = "SELECT VIDEO.id_user ,VIDEO.nome AS video_nome, SUM(VIEW.views) AS total_views
         FROM VIDEO
-        JOIN VIEW VIEW ON VIDEO.id = VIEW.id_video
+        JOIN VIEW ON VIDEO.id = VIEW.id_video
         GROUP BY VIDEO.id
         ORDER BY total_views DESC";
         $stmt = $this->connection->prepare($sql);
         $stmt->execute();
-        return $stmt->fetch(PDO::FETCH_ASSOC);
-    }
-
-    public function findByEmail(string $email): array {
-        $sql = "SELECT * FROM users WHERE email = :email";
-
-        $stmt = $this->connection->prepare($sql);
-        $stmt->bindParam(":email", $email);
-        $stmt->execute();
-
         return $stmt->fetch(PDO::FETCH_ASSOC);
     }
 }
