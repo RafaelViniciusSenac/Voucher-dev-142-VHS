cd7eec94de62afe035b1c67e38130f588cf5e3f0
Josué <josuealexandre067@gmail.com>
2025-03-21 17:14:22 -0400

    index.php

---
+++ src/views/components/card_de_evento.php
@@ -0,0 +1,27 @@
+<?php
+
+    //
+
+?>
+
+<!-- H T M L -->
+
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Card do Cake</title>
+
+    <script src="https://cdn.tailwindcss.com"></script>
+    <link rel="stylesheet" href="../../styles/global.css">
+</head>
+
+<body class="bg-[#0c0118] flex items-center justify-center min-h-screen p-10">
+
+    <div class="bg-gray-500 text-white ">
+        <!-- # -->
+    </div>
+
+</body>
+</html>
--- src/views/components/meu_card.php
+++ src/views/components/meu_card.php
@@ -43,28 +43,3 @@
     }
 
 ?>
-
-<!-- H T M L --> <!-- T E S T E -->
-
-<!DOCTYPE html>
-<html lang="pt-BR">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>❖ Meu card</title>
-    <link rel="stylesheet" href="../../styles/global.css">
-    <script src="https://cdn.tailwindcss.com"></script>
-</head>
-
-<body class="bg-[#0c0118] flex items-center justify-center min-h-screen p-10">
-    
-    <div class="flex flex-wrap gap-10 justify-center">
-        <?php 
-        foreach ($videos as $video) {
-            echo gerarMeuCard($video);
-        }
-        ?>
-    </div>
-
-</body>
-</html>
