da7c327dac8b3d82aba6f9c854468fb475738a4c
Kayke Adriano <kayke.adrianobdg@gmail.com>
2025-03-21 16:06:54 -0400

    card_cake

---
+++ src/views/components/card_evento_cake.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Card_Cake</title>
+    <script src="https://cdn.tailwindcss.com"></script> 
+</head>
+<body class="bg-[#0c0118] flex items-center justify-center min-h-screen p-10">
+    <div class="bg-gray-500 text-white ">aaqa</div>
+</body>
+</html>
