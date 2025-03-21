4770de50c3c2f8a33c82f661c281171750d2539e
Pedro Celestino <pedrojuniorcelestinocg@gmail.com>
2025-03-21 14:59:13 -0400

    feat: listar videos e estruturação controller

--- src/infra/models/videoModel.php
+++ src/infra/models/videoModel.php
@@ -5,21 +5,33 @@ require_once __DIR__ . '/../../infra/db/database.php';
 use Database;
 use PDO;
 
-class videoModel {
+class VideoModel {
     private PDO $connection;
 
     public function __construct() {
         $this->connection = (new Database())->getConnection();
     }
 
-    public function listMostPopularVideo(): string {
+    public function listMostPopularVideo(): array {
         $sql = "SELECT VIDEO.id_user ,VIDEO.nome AS video_nome, SUM(VIEW.views) AS total_views
         FROM VIDEO
         JOIN VIEW ON VIDEO.id = VIEW.id_video
         GROUP BY VIDEO.id
-        ORDER BY total_views DESC";
+        ORDER BY total_views DESC LIMIT 4";
         $stmt = $this->connection->prepare($sql);
         $stmt->execute();
-        return $stmt->fetch(PDO::FETCH_ASSOC);
+        return $stmt->fetchAll(PDO::FETCH_ASSOC);
+    }
+
+    public function listMostPopularVideoCategory(int $category_id): array{
+        $sql = "SELECT VIDEO.id_user ,VIDEO.nome AS video_nome, SUM(VIEW.views) AS total_views
+        FROM VIDEO
+        JOIN VIEW ON VIDEO.id = VIEW.id_video WHERE VIDEO.category_id = :category_id
+        GROUP BY VIDEO.id
+        ORDER BY total_views DESC LIMIT 8";
+        $stmt = $this->connection->prepare($sql);
+        $stmt->bindParam(":category_id", $category_id);
+        $stmt->execute();
+        return $stmt->fetchAll(PDO::FETCH_ASSOC);
     }
 }
