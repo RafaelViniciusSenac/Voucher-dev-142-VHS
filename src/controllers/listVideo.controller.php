8dd0b5e06a2832f01c0154df42d5ddbaac93934f
Pedro Celestino <pedrojuniorcelestinocg@gmail.com>
2025-05-20 16:57:37 -0400

    feat: terminei tudo eu acho

--- src/controllers/listVideo.controller.php
+++ src/controllers/listVideo.controller.php
@@ -6,14 +6,13 @@ use Src\Infra\Models\VideoModel;
 require_once __DIR__ . '/../infra/models/videoModel.php';
 
 class ListVideoController {
-    public function Handlee() {
+    public function handle() {
         $model = new VideoModel();
         try{
             $listVideos = $model->listMostPopularVideo();
             if (empty($listVideos)){
-                echo var_dump($listVideos);
                 http_response_code(400);
-                echo var_dump([
+                return[
                     "success" => false,
                     "errors" => [
                         "error" => true,
@@ -22,74 +21,19 @@ class ListVideoController {
                             "message" => "Lista vazia, videos não encontrados"
                             ]
                             ]
-                        ]);    
+                        ];    
                     }
-            echo var_dump("LISTA VIDEOS:",$listVideos);
             return $listVideos; 
 
         }catch(\Throwable $th){
             http_response_code(500);
-            echo var_dump([
+            return[
                 "success" => false,
                 "errors" => [
                     "error" => true,
                     "message" => "Erro interno no servidor"
                     ]
-                ]);
+                ];
             }
         }
-
-    public function listMostPopularVideoCategory(){
-        $model = new VideoModel();
-        try {
-            if (!isset($_POST["category_id"])){
-                http_response_code(400);
-                echo var_dump([
-                    "success" => false,
-                    "errors" => [
-                        "error" => true,
-                        "missing_fields" => [
-                            "error_code" => 1,
-                            "message" => "id da categoria não encontrado"
-                        ]
-                    ]
-                ]);
-            }
-
-            $listVideoCategory = $model->listMostPopularVideoCategory($_GET["category_id"]);
-
-            if (empty($listVideoCategory)){
-                http_response_code(400);
-                echo var_dump([
-                        "success" => false,
-                        "errors" => [
-                                    "error" => true,
-                                    "missing_fields" => [
-                                            "error_code" => 1,
-                                "message" => "Lista vazia, videos não encontrados"
-                            ]
-                        ]
-                    ]);
-                }
-
-            echo var_dump('LISTA CATEGORIA;',$listVideoCategory);
-            return $listVideoCategory;
-            
-        }
-        catch (\Throwable $th) {
-            http_response_code(500);
-            echo var_dump([
-                "success" => false,
-                "errors" => [
-                    "error" => true,
-                    "message" => "Erro interno no servidor"
-                    ]
-                ]);
-                return;
-            }  
-    }
 }
-
-$a = new ListVideoController();
-$_GET["category_id"];
-$a->listMostPopularVideoCategory();
